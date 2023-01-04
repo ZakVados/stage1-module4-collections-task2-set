f00549316c13a8a806c59bf27110151a3fafd66d
ZakVados <c0H7W2c3#mPZD$2Iuc4B0LbnruPGwi>
2023-01-04 12:12:16 +0200

    set task done

--- src/main/java/com/epam/mjc/collections/set/HashSetCreator.java
+++ src/main/java/com/epam/mjc/collections/set/HashSetCreator.java
@@ -5,5 +5,21 @@ import java.util.List;
 
 public class HashSetCreator {
     public HashSet<Integer> createHashSet(List<Integer> sourceList) {
+        HashSet<Integer> res = new HashSet<>();
+        for (Integer i : sourceList) {
+            if ((i % 2) == 0) {
+                do {
+                    res.add(i);
+                    i = i / 2;
+
+                } while (i % 2 == 0);
+                res.add(i);
+            } else {
+                res.add(i);
+                res.add(i * 2);
+            }
+        }
+        return res;
     }
+
 }
--- src/main/java/com/epam/mjc/collections/set/SubsetOfSquaresCreator.java
+++ src/main/java/com/epam/mjc/collections/set/SubsetOfSquaresCreator.java
@@ -2,8 +2,12 @@ package com.epam.mjc.collections.set;
 
 import java.util.List;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class SubsetOfSquaresCreator {
     public Set<Integer> createSubsetOfSquares(List<Integer> sourceList, int lowerBound, int upperBound) {
+        TreeSet<Integer> total = new TreeSet<>();
+        for (Integer i : sourceList) total.add(i*i);
+        return total.subSet(lowerBound, true, upperBound, true);
     }
 }
